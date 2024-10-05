464fd293efd60ae429cc6c4a35a4350afa292857
Charlie Zhuo <charliezm88@outlook.com>
2024-10-05 18:53:07 +0800

    实现基本的俄罗斯方块功能

--- src/main.cpp
+++ src/main.cpp
@@ -1,153 +1,323 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
+#include <queue>
+#include <vector>
 using namespace sf;
-
-const int M = 20;
-const int N = 10;
-
-int field[M][N] = {0};
+using namespace std;
 
 struct Point
-{int x,y;} a[4], b[4];
-
-int figures[7][4] =
 {
-	1,3,5,7, // I
-	2,4,5,7, // Z
-	3,5,4,6, // S
-	3,5,4,7, // T
-	2,3,5,7, // L
-	3,5,7,6, // J
-	2,3,4,5, // O
+	int x, y;
+} a[4], b[4];
+
+struct TetrominoType
+{
+	String name;
+	Color color;
+	std::vector<Point> pos; // 每个小方块在4*4的矩阵中的位置
+};
+TetrominoType AvailableTetrominoes[]{
+	{"I", Color::Red, {Point{0, 1}, Point{1, 1}, Point{2, 1}, Point{3, 1}}},
+	{"Z", Color::Green, {Point{0, 1}, Point{1, 1}, Point{1, 0}, Point{2, 0}}},
+	{"S", Color::Blue, {Point{0, 0}, Point{1, 0}, Point{1, 1}, Point{2, 1}}},
+	{"T", Color::Yellow, {Point{1, 0}, Point{0, 1}, Point{1, 1}, Point{2, 1}}},
+	{"L", Color::Magenta, {Point{0, 1}, Point{1, 1}, Point{2, 1}, Point{2, 0}}},
+	{"J", Color::Cyan, {Point{0, 0}, Point{0, 1}, Point{1, 1}, Point{2, 1}}},
+	{"O", Color(100, 120, 140, 255), {Point{0, 0}, Point{1, 0}, Point{0, 1}, Point{1, 1}}}};
+
+enum class MoveDirection
+{
+	Down,
+	Left,
+	Right,
+	Rotate
 };
 
-bool check()
+class Tetromino
 {
-   for (int i=0;i<4;i++)
-	  if (a[i].x<0 || a[i].x>=N || a[i].y>=M) return 0;
-      else if (field[a[i].y][a[i].x]) return 0;
+public:
+	Tetromino()
+	{
+		int n = rand() % 7;
+		tetrominoType = AvailableTetrominoes[n];
+		currentPosition = tetrominoType.pos;
+	};
 
-   return 1;
+	// 获取当前方块的移动后各个小方块的位置
+	std::vector<Point> calculateMoveDestination(MoveDirection moveDirection)
+	{
+		std::vector<Point> moveDestination=currentPosition;
+		switch (moveDirection)
+		{
+		case MoveDirection::Down:
+			for (size_t i = 0; i < 4; i++)
+			{
+				moveDestination[i].y++;
+			}
+			break;
+		case MoveDirection::Left:
+			for (size_t i = 0; i < 4; i++)
+			{
+				moveDestination[i].x--;
+			}
+			break;
+		case MoveDirection::Right:
+			for (size_t i = 0; i < 4; i++)
+			{
+				moveDestination[i].x++;
+			}
+			break;
+		case MoveDirection::Rotate: // 绕4*4矩阵的中心旋转90度
+			Point center = currentPosition[1];
+			for (size_t i = 0; i < 4; i++)
+			{
+				int x = currentPosition[i].y - center.y;
+				int y = currentPosition[i].x - center.x;
+				moveDestination[i].x = center.x - x;
+				moveDestination[i].y = center.y + y;
+			}
+			break;
+		};
+		return moveDestination;
+	};
+
+	void setPosition(std::vector<Point> pos)
+	{
+		currentPosition = pos;
+	};
+
+	std::vector<Point> getCurrentPosition()
+	{
+		return currentPosition;
+	};
+
+	TetrominoType getType()
+	{
+		return tetrominoType;
+	}
+
+private:
+	TetrominoType tetrominoType;
+	std::vector<Point> currentPosition;
 };
 
+class Board
+{
+public:
+	static const int BoardHeight = 20;
+	static const int BoardWidth = 10;
+
+	Board()
+	{
+		activeTetromino = Tetromino();
+		futureTetrominoes.push(Tetromino());
+		futureTetrominoes.push(Tetromino());
+		futureTetrominoes.push(Tetromino());
+	};
+
+	// 检查是否与边界或其他方块碰撞
+	bool willCollide(std::vector<Point> moveDestination)
+	{
+		for (size_t i = 0; i < moveDestination.size(); i++)
+		{
+			if (moveDestination.at(i).x < 0 || moveDestination.at(i).x >= BoardWidth || moveDestination.at(i).y >= BoardHeight)
+				return true;
+			else if (field[moveDestination.at(i).y][moveDestination.at(i).x] != Color::Black)
+				return true;
+		}
+		return false;
+	};
+
+	// 检查指定行已经填满
+	bool checkLine(int lineNum)
+	{
+		for (int i = 0; i < BoardWidth; i++)
+		{
+			if (field[lineNum][i] == Color::Black)
+			{
+				return false;
+			}
+		}
+		return true;
+	};
+
+	// 消除指定行内容，并将上面的方块下移
+	void clearLine(int lineNum)
+	{
+		for (int i = 0; i < BoardWidth; i++)
+		{
+			field[lineNum][i] = Color::Black;
+		}
+		for (int i = lineNum; i > 0; i--)
+		{
+			for (int j = 0; j < BoardWidth; j++)
+			{
+				field[i][j] = field[i - 1][j];
+			}
+		}
+	};
+
+	// 检查所有行是否有填满的，有则消除
+	void clearLines()
+	{
+		for (int i = 0; i < BoardHeight; i++)
+		{
+			while (checkLine(i))
+			{
+				clearLine(i);
+			}
+		}
+	};
+
+	// 固定当前方块，将其加入到field中，并生成新的方块作为活动方块
+	void fixTetromino()
+	{
+		std::vector<Point> currentPosition = activeTetromino.getCurrentPosition();
+		for (size_t i = 0; i < currentPosition.size(); i++)
+		{
+			field[currentPosition[i].y][currentPosition[i].x] = activeTetromino.getType().color;
+		}
+
+		activeTetromino = futureTetrominoes.front();
+		futureTetrominoes.pop();
+
+		futureTetrominoes.push(Tetromino());
+	};
+
+	bool isGameOver()
+	{ // 检查最上面一行是否有方块
+		for (int i = 0; i < BoardWidth; i++)
+		{
+			if (field[0][i] != Color::Black)
+			{
+				return true;
+			}
+		}
+		return false;
+	};
+
+	void draw(RenderWindow &window)
+	{
+		window.clear(Color::Black);
+
+		// 画边界
+		RectangleShape border(Vector2f(180, 360));
+		border.setFillColor(Color::Black);
+		border.setOutlineColor(Color::White);
+		border.setOutlineThickness(2);
+		border.setPosition(28, 31);
+		window.draw(border);
+
+		// 画field
+		for (int i = 0; i < BoardHeight; i++)
+		{
+			for (int j = 0; j < BoardWidth; j++)
+			{
+				if (field[i][j] == Color::Black)
+					continue;
+				RectangleShape rect(Vector2f(18, 18));
+				rect.setFillColor(field[i][j]);
+				rect.setOutlineColor(Color::Black);
+				rect.setOutlineThickness(1);
+				rect.setPosition(j * 18, i * 18);
+				rect.move(28, 31); // offset
+				window.draw(rect);
+			}
+		}
+
+		// 画当前方块
+		for (int i = 0; i < 4; i++)
+		{
+			RectangleShape rect(Vector2f(18, 18));
+			rect.setFillColor(activeTetromino.getType().color);
+			rect.setOutlineColor(Color::Black);
+			rect.setOutlineThickness(1);
+			rect.setPosition(activeTetromino.getCurrentPosition()[i].x * 18, activeTetromino.getCurrentPosition()[i].y * 18);
+			rect.move(28, 31); // offset
+			window.draw(rect);
+		}
+		window.display();
+	}
+
+	// 定时运行
+	void tick()
+	{
+		std::vector<Point> pts =
+			activeTetromino.calculateMoveDestination(MoveDirection::Down);
+		if (willCollide(pts))
+		{
+			fixTetromino();
+			clearLines();
+		}
+		else
+		{
+			activeTetromino.setPosition(pts);
+		}
+	}
+
+	void handleKeyEvent(Event e)
+	{
+		if (e.type == Event::KeyPressed)
+		{
+			std::vector<Point> pts;
+			if (e.key.code == Keyboard::Up)
+			{
+				pts = activeTetromino.calculateMoveDestination(MoveDirection::Rotate);
+			}
+			else if (e.key.code == Keyboard::Left)
+			{
+				pts = activeTetromino.calculateMoveDestination(MoveDirection::Left);
+			}
+			else if (e.key.code == Keyboard::Right)
+			{
+				pts = activeTetromino.calculateMoveDestination(MoveDirection::Right);
+			}
+			else if (e.key.code == Keyboard::Down)
+			{
+				pts = activeTetromino.calculateMoveDestination(MoveDirection::Down);
+			}
+			if (!willCollide(pts))
+			{
+				activeTetromino.setPosition(pts);
+			}
+		}
+	}
+
+private:
+	// 20*10的矩阵，每个元素表示一个方块的颜色
+	// 最上方的行号为0，最左边的列号为0
+	Color field[BoardHeight][BoardWidth] = {Color::Black}; // Black表示空，其他颜色表示有方块
+	Tetromino activeTetromino;
+	std::queue<Tetromino> futureTetrominoes;
+};
 
 int main()
 {
-    srand(time(0));	 
-
-	RenderWindow window(VideoMode(320, 480), "The Game!");
-
-    Texture t1,t2,t3;
-	t1.loadFromFile("images/tiles.png");
-	t2.loadFromFile("images/background.png");
-	t3.loadFromFile("images/frame.png");
-
-	Sprite s(t1), background(t2), frame(t3);
-
-    int dx=0; bool rotate=0; int colorNum=1;
-	float timer=0,delay=0.3; 
-
+	Time tickInterval = milliseconds(500);
 	Clock clock;
 
-    while (window.isOpen())
-    {
-		float time = clock.getElapsedTime().asSeconds();
-		clock.restart();
-		timer+=time;
+	RenderWindow window(VideoMode(320, 480), "The Game!");
+	Board board;
 
-        Event e;
-        while (window.pollEvent(e))
-        {
-            if (e.type == Event::Closed)
-                window.close();
-
-			if (e.type == Event::KeyPressed)
-			  if (e.key.code==Keyboard::Up) rotate=true;
-			  else if (e.key.code==Keyboard::Left) dx=-1;
-			  else if (e.key.code==Keyboard::Right) dx=1;
-		}
-
-	if (Keyboard::isKeyPressed(Keyboard::Down)) delay=0.05;
-
-	//// <- Move -> ///
-	for (int i=0;i<4;i++)  { b[i]=a[i]; a[i].x+=dx; }
-    if (!check()) for (int i=0;i<4;i++) a[i]=b[i];
-
-	//////Rotate//////
-	if (rotate)
-	  {
-		Point p = a[1]; //center of rotation
-		for (int i=0;i<4;i++)
-		  {
-			int x = a[i].y-p.y;
-			int y = a[i].x-p.x;
-			a[i].x = p.x - x;
-			a[i].y = p.y + y;
-	 	  }
-   	    if (!check()) for (int i=0;i<4;i++) a[i]=b[i];
-	  }
-
-	///////Tick//////
-	if (timer>delay)
-	  {
-	    for (int i=0;i<4;i++) { b[i]=a[i]; a[i].y+=1; }
-
-		if (!check())
-		{
-		 for (int i=0;i<4;i++) field[b[i].y][b[i].x]=colorNum;
-
-		 colorNum=1+rand()%7;
-		 int n=rand()%7;
-		 for (int i=0;i<4;i++)
-		   {
-		    a[i].x = figures[n][i] % 2;
-		    a[i].y = figures[n][i] / 2;
-		   }
-		}
-
-	  	timer=0;
-	  }
-
-	///////check lines//////////
-    int k=M-1;
-	for (int i=M-1;i>0;i--)
+	while (window.isOpen())
 	{
-		int count=0;
-		for (int j=0;j<N;j++)
+
+		Event e;
+		while (window.pollEvent(e))
 		{
-		    if (field[i][j]) count++;
-		    field[k][j]=field[i][j];
+			if (e.type == Event::Closed)
+				window.close();
+			board.handleKeyEvent(e);
+			board.draw(window);
 		}
-		if (count<N) k--;
+		Time elapsed = clock.getElapsedTime();
+		if (elapsed > tickInterval)
+		{
+			board.tick();
+			board.draw(window);
+			clock.restart();
+		}
 	}
 
-    dx=0; rotate=0; delay=0.3;
-
-    /////////draw//////////
-    window.clear(Color::White);	
-    window.draw(background);
-		  
-	for (int i=0;i<M;i++)
-	 for (int j=0;j<N;j++)
-	   {
-         if (field[i][j]==0) continue;
-		 s.setTextureRect(IntRect(field[i][j]*18,0,18,18));
-		 s.setPosition(j*18,i*18);
-		 s.move(28,31); //offset
-		 window.draw(s);
-	   }
-
-	for (int i=0;i<4;i++)
-	  {
-		s.setTextureRect(IntRect(colorNum*18,0,18,18));
-		s.setPosition(a[i].x*18,a[i].y*18);
-		s.move(28,31); //offset
-		window.draw(s);
-	  }
-
-	window.draw(frame);
- 	window.display();
-	}
-
-    return 0;
+	return 0;
 }
